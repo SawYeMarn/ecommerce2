787a4c9a8853d3cac2283af0d909eb2009fdad43
Ye Marn Paing <yemarnpaing44@gmail.com>
2025-02-22 15:14:12 +0630

    second commit

---
+++ resources/views/components/relatedproduct.blade.php
@@ -0,0 +1,20 @@
+<a href="/productDetail.html">
+                <div class="flex items-center gap-5 group cursor-pointer">
+                  <div class="basis-[30%] h-auto rounded-xl overflow-hidden">
+                    <img
+                      class="w-full h-full group-hover:scale-[1.1] transition-all duration-200 object-cover"
+                      src="https://cdn.prod.website-files.com/62f51a90d298e65b94bbffcd/62f6a67c4666f047ada3ba87_image-10-shop-product-shopwave-template-p-500.png"
+                    />
+                  </div>
+                  <div class="basis-[70%]">
+                    <p
+                      class="text-lg group-hover:text-primary transition-all duration-200 font-semibold"
+                    >
+                      Product Name
+                    </p>
+                    <div class="flex items-center gap-2">
+                      <p class="font-bold">100000 MMK</p>
+                    </div>
+                  </div>
+                </div>
+              </a>
